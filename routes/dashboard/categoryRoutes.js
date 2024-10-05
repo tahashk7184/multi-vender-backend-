1bfa99e56e5ac7dee390ba9f8ee55a6e8ef92181
tahashk7184 <tahashk7184@gmail.com>
2024-10-05 14:04:30 +0500

    First commit for Glitch

---
+++ routes/dashboard/categoryRoutes.js
@@ -0,0 +1,9 @@
+const router = require('express').Router();
+const { authMiddleware } = require('../../middlewares/authMiddleware');
+const categoryController = require('../../controllers/dashboard/categoryController');
+
+router.post('/category-add', authMiddleware, categoryController.add_category);
+router.get('/category-get', authMiddleware, categoryController.get_category);
+router.delete('/category-delete/:id', categoryController.delete_category);
+
+module.exports = router;
